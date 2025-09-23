8db3aa921e177c35f440afeb402dbb348eeae90a
Timokrut <potapov.tim14@gmail.com>
2025-09-23 16:09:48 +0300

    collect all metadata to _metadata + new route to get it

--- server.py
+++ server.py
@@ -7,6 +7,8 @@ import os
 
 app = FastAPI()
 
+_metadata = dict()
+
 @app.post("/metadata/")
 async def extract_metadata(file: UploadFile = File(...)):
     with tempfile.NamedTemporaryFile(delete=False) as tmp:
@@ -28,9 +30,21 @@ async def extract_metadata(file: UploadFile = File(...)):
             )
 
         metadata = json.loads(result.stdout)[0] if result.stdout else {}
+
+        for i in metadata.keys():
+            if i not in _metadata:
+                _metadata[i] = 1
+            else: 
+                _metadata[i] += 1            
+
+            
         return JSONResponse(content=metadata)
 
     finally:
         if os.path.exists(tmp_path):
             os.remove(tmp_path)
+
+@app.get("/get-metadata")
+def get_metadata():
+    return sorted(_metadata.items(), key=lambda x: x[1])
 
